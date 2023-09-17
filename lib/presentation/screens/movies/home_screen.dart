526ce8cdb8e8bca7a1ba0829817ca0f2cc159a11
Juan Nacl <jgarciaot@unal.edu.co>
2023-09-17 16:40:34 -0500

    Agregar apiKye

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:flutter/material.dart';
+
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,8 +8,10 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const  Scaffold(
-      body: Placeholder()
+    return   Scaffold(
+      body: Center(
+        child:  Text(environment.theMovieDbKey),
+      )
     );
   }
 }
