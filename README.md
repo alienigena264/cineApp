b5747e9da989251b2f7a801c60d1a225b25027d2
Juan Nacl <jgarciaot@unal.edu.co>
2023-09-17 16:21:25 -0500

    Añadir .env

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # cinemapedia
 
-A new Flutter project.
+#Dev
+
+1. Copiar y renombrar el .env.template
+2. Asignar su propio apiKey de moviedb
+
+
---
+++ lib/domain/repositories/movires_repository.dart
@@ -0,0 +1,7 @@
+
+import 'package:cinemapedia/domain/entities/movie.dart';
+
+abstract class MovieRepository {
+  Future<List<Movie>>getNowPlayingMovies({int page = 1});
+
+}
