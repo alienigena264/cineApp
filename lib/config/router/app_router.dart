da545935bc8a5cc33dde6664552bd7ea1230f537
Juan Nacl <jgarciaot@unal.edu.co>
2023-11-20 11:47:07 -0500

    Go-router oficial

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,29 +1,52 @@
 import 'package:go_router/go_router.dart';
 
+import 'package:cinemapedia/presentation/views/views.dart';
 import 'package:cinemapedia/presentation/screens/screens.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/',
-  routes: [
-    
-    GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
+final appRouter = GoRouter(initialLocation: '/', routes: [
+  ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
       routes: [
-         GoRoute(
-          path: 'movie/:id',
-          name: MovieScreen.name,
-          builder: (context, state) {
-            final movieId = state.params['id'] ?? 'no-id';
+        GoRoute(
+            path: '/',
+            builder: ((context, state) {
+              return const HomeView();
+            }),
+            routes: [
+              GoRoute(
+                path: 'movie/:id',
+                name: MovieScreen.name,
+                builder: (context, state) {
+                  final movieId = state.params['id'] ?? 'no-id';
 
-            return MovieScreen( movieId: movieId );
-          },
+                  return MovieScreen(movieId: movieId);
+                },
+              ),
+            ]),
+        GoRoute(
+          path: '/favorites',
+          builder: ((context, state) {
+            return const FavoritesView();
+          }),
         ),
-      ]
-    ),
+      ])
 
+  //Rutas directas/ sin implementar el bottom nav bar
+  // GoRoute(
+  //     path: '/',
+  //     name: HomeScreen.name,
+  //     builder: (context, state) => const HomeScreen(childView: HomeView()),
+  //     routes: [
+  // GoRoute(
+  //   path: 'movie/:id',
+  //   name: MovieScreen.name,
+  //   builder: (context, state) {
+  //     final movieId = state.params['id'] ?? 'no-id';
 
-
-  ]
-);
+  //     return MovieScreen(movieId: movieId);
+  //   },
+  // ),
+  //     ]),
+]);
---
+++ lib/presentation/views/views.dart
@@ -0,0 +1,2 @@
+export 'package:cinemapedia/presentation/views/home_views/favorite_view.dart';
+export 'package:cinemapedia/presentation/views/home_views/home_view.dart';
