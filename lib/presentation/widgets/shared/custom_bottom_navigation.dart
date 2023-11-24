36a362fb465a747cb4731ea7fafca47b774a6e9f
Juan Nacl <jgarciaot@unal.edu.co>
2023-11-23 20:43:43 -0500

    Permanencia estado

--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
-
+import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigation extends StatelessWidget {
-  const CustomBottomNavigation({super.key});
+  const CustomBottomNavigation({super.key, required this.currentIndex});
+  final int currentIndex;
+  void onItemTap(BuildContext context, int index) {
+    // final router = GoRouter.of(context);
+    context.go('/home/$index');
+  }
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      elevation: 0,
-      items: const [
-        BottomNavigationBarItem(
-          icon: Icon( Icons.home_max ),
-          label: 'Inicio'
-        ),
-        BottomNavigationBarItem(
-          icon: Icon( Icons.label_outline ),
-          label: 'Categorías'
-        ),
-        BottomNavigationBarItem(
-          icon: Icon( Icons.favorite_outline ),
-          label: 'Favoritos'
-        ),
-      ]
-    );
+        currentIndex: currentIndex,
+        onTap: (index) => onItemTap(context, index),
+        elevation: 0,
+        selectedItemColor: Colors.pink,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Inicio'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.label_outline), label: 'Categorías'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_outline), label: 'Favoritos'),
+        ]);
   }
 }
