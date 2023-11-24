39de377e0674bb1bb7e64489876299305c1acd8c
Juan Nacl <jgarciaot@unal.edu.co>
2023-11-24 00:21:50 -0500

    Preparacion domain e infraestructure

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 
 1. Copiar y renombrar el .env.template
 2. Asignar su propio apiKey de moviedb
-
+3: cambios en la entidad usar flutter pub run build_runner build
 
---
+++ lib/infrastructure/repositories/local_storage_impl.dart
@@ -0,0 +1,29 @@
+
+
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/domain/repositories/local_storage_repository.dart';
+
+import '../../domain/datasource/local_storage_datasouse.dart';
+
+class LocalStorageRepositoryImpl extends LocalStorageRepository {
+
+  
+  final LocalStorageDataSource dataSource;
+
+  LocalStorageRepositoryImpl({required this.dataSource});
+  @override
+  Future<bool> isMovieFavorite(int movieId) {
+    return dataSource.isMovieFavorite(movieId);
+  }
+
+  @override
+  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) {
+    return dataSource.loadMovies(limit: limit, offset: offset);
+  }
+
+  @override
+  Future<void> toggleFavorite(Movie movie) {
+    return dataSource.toggleFavorite(movie);
+  }
+
+} 
